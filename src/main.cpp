c20120c378a34d22947d5690cefa88fbce497f79
Marco Schmid <marco.ms.schmid@gmail.com>
2016-04-02 21:39:43 +0200

    initial

---
+++ src/main.cpp
@@ -0,0 +1,106 @@
+/**
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+#include <Arduino.h>
+#include <DHT.h>
+#include <Wire.h>
+#include <SeeedOLED.h>
+#include <SFE_BMP180.h>
+
+#define DHTPIN 2 //temp sensonr
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+SFE_BMP180 pressure;
+double baseline = 1013.25; // baseline pressure
+
+double getPressure();
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("DHTxx test!");
+
+    Wire.begin();
+    SeeedOled.init();
+
+    SeeedOled.clearDisplay();
+    SeeedOled.setNormalDisplay();
+    SeeedOled.setPageMode();
+
+    SeeedOled.setTextXY(0,0);
+    SeeedOled.putString("Weatherino");
+    SeeedOled.setTextXY(7,0);
+    SeeedOled.putString("Marco Schmid");
+
+    SeeedOled.setTextXY(2,0);
+    SeeedOled.putString("T:");
+    SeeedOled.setTextXY(2,11);
+    SeeedOled.putString("C");
+
+    SeeedOled.setTextXY(3,0);
+    SeeedOled.putString("H:");
+    SeeedOled.setTextXY(3,11);
+    SeeedOled.putString("%");
+
+    SeeedOled.setTextXY(4,0);
+    SeeedOled.putString("P:");
+    SeeedOled.setTextXY(4,11);
+    SeeedOled.putString("mbar");
+
+    SeeedOled.setTextXY(5,0);
+    SeeedOled.putString("A:");
+    SeeedOled.setTextXY(5,11);
+    SeeedOled.putString("m");
+
+    dht.begin();
+    pressure.begin();
+}
+
+double tese()
+{
+    return 0.0;
+}
+
+void loop()
+{
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    double P = getPressure();
+    double a = pressure.altitude(P,baseline);
+
+    SeeedOled.setTextXY(2,4);
+    SeeedOled.putFloat(t);
+    SeeedOled.setTextXY(3,4);
+    SeeedOled.putFloat(h);
+    SeeedOled.setTextXY(4,4);
+    SeeedOled.putFloat(P);
+    SeeedOled.setTextXY(5,4);
+    SeeedOled.putFloat(a);
+    delay(2000);
+}
+
+double getPressure()
+{
+    char status;
+    double T,P;
+
+    status = pressure.startTemperature();
+    if (status != 0)
+    {
+        delay(status);
+        status = pressure.getTemperature(T);
+        if (status != 0)
+        {
+            status = pressure.startPressure(3);
+            if (status != 0)
+            {
+                delay(status);
+                status = pressure.getPressure(P,T);
+                return P;
+            }
+        }
+    }
+}
